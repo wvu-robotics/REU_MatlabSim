388d384a3bbffe33378e7ee9c7b7d7de6389bc00
DavidRubel <51805103+DavidRubel@users.noreply.github.com>
2021-06-11 11:18:21 -0400

    Fixed merge problems

--- matlab/Collision Avoidance Sim/BasicSim.m
+++ matlab/Collision Avoidance Sim/BasicSim.m
@@ -2,7 +2,7 @@ clear
 clc
 close all
 
-numberOfAgents = 2;
+numberOfAgents = 7;
 agentRadius = 1;
 mapSize = 10;
 timeStep = .05;
@@ -20,31 +20,36 @@ accelerationConstant = .5;
 % agentPositions = [-7,-8;8,-8];
 % goalLocations = [8,8;-8,8];
 
-%Random positions around a circle
-% agentPositions = zeros(numberOfAgents, 2);
-% goalLocations = zeros(numberOfAgents, 2);
-% for i = 1:numberOfAgents
-% theta = rand()*2*pi;
-% agentPositions(i,:) = [cos(theta),sin(theta)]*mapSize*(.7+(rand()-0.5)*.2);
-% goalLocations(i,:) = [cos(theta+pi),sin(theta+pi)]*mapSize*(.7+(rand()-0.5)*.2);
-% end
-
+% Random positions around a circle
 agentPositions = zeros(numberOfAgents, 2);
 goalLocations = zeros(numberOfAgents, 2);
 for i = 1:numberOfAgents
-    theta = 2*pi/numberOfAgents * (i-1);
-    agentPositions(i,:) = [cos(theta),sin(theta)]*mapSize*(.9+(rand()-0.5)*.1);
-    goalLocations(i,:) = [cos(theta+3.9*pi/4),sin(theta+3.9*pi/4)]*mapSize*(.9+(rand()-0.5)*.1);
+theta = rand()*2*pi;
+agentPositions(i,:) = [cos(theta),sin(theta)]*mapSize*(.7+(rand()-0.5)*.2);
+goalLocations(i,:) = [cos(theta+pi),sin(theta+pi)]*mapSize*(.7+(rand()-0.5)*.2);
 end
+
+% agentPositions = zeros(numberOfAgents, 2);
+% goalLocations = zeros(numberOfAgents, 2);
+% for i = 1:numberOfAgents
+%     theta = 2*pi/numberOfAgents * (i-1);
+%     agentPositions(i,:) = [cos(theta),sin(theta)]*mapSize*(.9+(rand()-0.5)*.1);
+%     goalLocations(i,:) = [cos(theta+3.9*pi/4),sin(theta+3.9*pi/4)]*mapSize*(.9+(rand()-0.5)*.1);
+% end
 
 agentVelocities = zeros(numberOfAgents,2);
 path = zeros(length(0:timeStep:maxTime)-1,2,numberOfAgents);
 counter = 0;
 collisions = 0;
 
-VOenv = velocityObstacleEnv(numberOfAgents,1);
+VOenv = velocityObstacleEnv(numberOfAgents);
 VOenv = VOenv.setRT(2*agentRadius,timeHorizon);
-VOenv = VOenv.setPlot(4,4);
+VOenv = VOenv.setPlot(1,1,1);
+VOenv = VOenv.addVector(1,'r',1);
+
+for i = 2:numberOfAgents
+    VOenv = VOenv.addGraphicsVO(1,i);
+end
 
 figPS = figure('Name', 'Position Space');
 axis([-mapSize mapSize -mapSize mapSize])
@@ -71,15 +76,11 @@ for t = 0:timeStep:maxTime
         path(counter,:,i) = agentPositions(i, :);
     end
     %Computes what velocity each robot wants to take
-    prefVelocities = (goalLocations - agentPositions)./vecnorm(goalLocations - agentPositions, 2, 2) * maxVelocity;
+    velInput = (goalLocations - agentPositions)./vecnorm(goalLocations - agentPositions, 2, 2) * maxVelocity;
     
-    velocityControls = ORCAController(agentPositions, agentVelocities, prefVelocities, timeHorizon, sensingRange, agentRadius, maxVelocity, velocityDiscritisation, vOptIsZero, responsibility);
+    velocityControls = ORCAController(agentPositions, agentVelocities, velInput, timeHorizon, sensingRange, agentRadius, maxVelocity, velocityDiscritisation, vOptIsZero, responsibility);
     
-    accelerationInputs = potentField(agentPositions, agentVelocities, velocityControls, sensingRange, agentRadius, safetyMargin);
-    agentVelocities = agentVelocities + accelerationConstant * accelerationInputs * timeStep;
-    
-    %Handles collisions
-    [agentVelocities, numCollisions] = Collider(agentPositions, agentVelocities, agentRadius, timeStep);
+    [agentVelocities, numCollisions] = Collider(agentPositions, velocityControls, agentRadius, timeStep);
     
     %Increments velocity, then position
     agentPositions =  agentPositions + agentVelocities * timeStep;
@@ -98,9 +99,10 @@ for t = 0:timeStep:maxTime
                                              agentPositions(i,2)]);
     end
     pause(0.01)
-   
-    VOenv.drawVector(agentVelocities(1,:),1);
-    VOenv.displayVO(agentPositions',agentVelocities',1);
+    
+    VOenv = VOenv.setVO(agentPositions',1);
+    VOenv.drawVector([0, agentVelocities(1,1), 0, agentVelocities(1,2)],1,1);
+    VOenv.displayAgentVO(1,agentVelocities');
    
     if max(vecnorm(agentPositions - goalLocations,2,2)) < 0.2
         break; 
