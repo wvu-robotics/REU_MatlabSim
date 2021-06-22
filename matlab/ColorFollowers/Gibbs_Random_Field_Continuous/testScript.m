8c77c4a70758ae1997d8e09db9310bd92340c4ab
sej0015 <60272968+sej0015@users.noreply.github.com>
2021-06-22 09:56:21 -0400

    Small changes to GRF method

---
+++ matlab/ColorFollowers/Gibbs_Random_Field_Continuous/testScript.m
@@ -0,0 +1,16 @@
+
+
+radius = 2.0:0.01:5;
+epsilon = 0.04;
+epsilon_not = 0.04;
+alpha = 1;
+r_not = 8;
+mass = 0.1;
+        
+chargeProduct = -16;
+        
+CBPotential = (epsilon .* (6./(alpha - 6) .* exp(alpha)...
+                    * (1 - radius./r_not) - alpha./(alpha - 6) .* (r_not./radius).^6)...
+                    + chargeProduct./(4.*pi.*epsilon_not.*radius));
+                
+plot(radius, CBPotential);
