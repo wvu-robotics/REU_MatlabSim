aa530d22c5c7277f8be5cec216f6e20adfde9ea0
sej0015 <60272968+sej0015@users.noreply.github.com>
2021-06-28 18:22:44 -0400

    Made the vector optimization a function that outputs a cost

--- matlab/ColorFollowers/Vector Optimization/VectorOptimizationRun.m
+++ matlab/ColorFollowers/Vector Optimization/VectorOptimizationRun.m
@@ -1,22 +1,23 @@
-clear
-clc
-close all
+function cost = VectorOptimizationRun(parameters)
 
 %   World Building
 numberOfAgents = 30;
 agentRadius = .5;
-timeStep = .05;
-mapSize = 20;
+timeStep = .1;
+mapSize = 15;
 counter = 0;
 maxSpeed = 1;
 sensingRange = 5;
 
-%spawnType = 'random';
-spawnType = 'opposingGroups';
+% Parameters for the cost function
+timeCost = 0.1;
+relativeVelCost = 0.01;
+collisionCost = 0.1;
+connectivityBenefit = -0.01;
 
-paramRows = 5;
-paramCols = 4;
-parameters = (rand([5,4])-0.5)*10;
+spawnType = 'random';
+%spawnType = 'opposingGroups';
+
 
 f = @ColorVectorController;
 
@@ -43,6 +44,8 @@ end
 ENV.setAgentPositions(initPositions);
 ENV.setGoalPositions(goalLocations);
 ENV.setAgentVelocities(zeros(numberOfAgents,2));
+ENV.realTime = false;
+ENV.pathVisibility(false)
 
 for i = 1:numberOfAgents
     ENV.agents(i).setController(f);
@@ -50,22 +53,35 @@ for i = 1:numberOfAgents
     ENV.agents(i).measuringRange = sensingRange;
     ENV.agents(i).maxSpeed = maxSpeed;
 end 
-ENV.pathVisibility(true);
 
+
+cost = 0;
 while(true)
     ENV.tick;
     counter = counter + 1;
     fprintf("Time: %i \n",counter)
-    if counter > 2000
+    if counter > 100
        break 
     end
     ENV.agents(1).getTimeStep;
+    
+    %Evaluate Some of the cost for each time step
+    for i = 1:length(ENV.agents)
+        if norm(ENV.agents(i).pose - ENV.agents(i).goalPose) > 0.5
+            cost = cost + timeCost;
+        end
+        for j = 1:length(ENV.agents(i).measuredAgents)
+            relSpeed = norm(ENV.agents(i).velocity - ENV.agents(i).measuredAgents(j).velocity);
+            cost = cost + relSpeed*relativeVelCost;
+        end
+        cost = cost + connectivityBenefit * length(ENV.agents(i).measuredAgents);
+    end
 end
-ENV.collisions
+fprintf('Collisions: %.0f\n', ENV.collisions);
+cost = cost+collisionCost*ENV.collisions;
+fprintf('Cost: %.3f\n', cost);
 
 
-
-
-
+end
 
 
