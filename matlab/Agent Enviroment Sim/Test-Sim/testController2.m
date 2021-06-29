6544279b1120e73d13e6bca3f722763b126298e3
rmb0034 <rmb0034@mix.wvu.edu>
2021-06-29 10:10:54 -0400

    Test controller

---
+++ matlab/Agent Enviroment Sim/Test-Sim/testController2.m
@@ -0,0 +1,16 @@
+function testController(agent) 
+   if ~isempty(agent.measuredAgents)
+       agent.color = 'r';
+        for i = 1:length(agent.measuredAgents)
+            x = agent.pose - agent.measuredAgents(i).pose;
+            x = x/norm(x);
+            agent.velocityControl = 5*x;
+           
+        end 
+   else
+       agent.velocityControl = 0.1*[1 1]; 
+       agent.color = 'g';
+   end
+   agent.getProperty('wallet')
+end
+
