93b90759e92d5f665b868a9af95a459914745dc9
dq621 <dqishmon621@gmail.com>
2021-07-08 17:10:18 -0400

    Swarm invader

--- matlab/Agent Enviroment Sim/agentEnv 2.0/Controllers/Human Interference Scripts/testController5.m
+++ matlab/Agent Enviroment Sim/agentEnv 2.0/Controllers/Human Interference Scripts/testController5.m
@@ -2,7 +2,7 @@ function testController5(Agent1)
 Agent1.color = [0 1 0];
 for i = 1:length(Agent1.measuredAgents)
    if Agent1.measuredAgents(i).getProperty('isEnemy') == true
-       x = objectFlow1(Agent1.measuredAgents(i).pose, Agent1.pose,1);
+       x = objectFlow1(Agent1.measuredAgents(i).pose, Agent1.pose,1000000);
       Agent1.velocityControl= 5*(x/norm(x));
       % x  = Agent1.pose - Agent1.measuredAgents(i).pose -0.5;
    end
--- matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/Human Interference Scripts/envTestDemoHumanInterferenceSwarm.m
+++ matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/Human Interference Scripts/envTestDemoHumanInterferenceSwarm.m
@@ -2,13 +2,14 @@ clc
 clear
 close all
 %   World Building
-numberOfAgents = 7;
+numberOfAgents = 6;
 agentRadius = .5;
 timeStep = .05;
-mapSize = 20;
+mapSize = 25;
 counter = 0;
- shape = circle (.5)
-%  *[-2,-1;-2,1;2,1;2,-1];
+shape = circle (.75);
+% shape=.25*[-2,-1;-2,1;2,1;2,-1];
+safetyMargin = 5;
 
 
 f(1)={@testControllerEnemy1};
@@ -25,8 +26,23 @@ for i = 1:numberOfAgents
 end
     ENV.agents(1).setProperty('isEnemy', true);
 %Setting Initial Positions
-initPositions = [-8,-9;-8,-7;-8,-5;-8,-3;-8,-1;-8,1;-8,3];
-goalLocations = 1.*ones(numberOfAgents,2);
+initPositions = [-8,-8;-8,-6;-8,-4;-8,-2;-8,0;-8,2;-8,4];
+% initPositions = zeros(numberOfAgents,2);
+% initpositions(1,:) = [0,0];
+% possCo = (agentRadius-mapSize):(2*agentRadius*safetyMargin):(mapSize-2*agentRadius);
+% for i = 3:min(length(possCo),numberOfAgents)
+%     initPositions(i,:) = [agentRadius-mapSize,possCo(i)];
+% end
+% for i = (length(possCo)+1):min(2*length(possCo),numberOfAgents)
+%     initPositions(i,:) = [possCo(i-length(possCo)),mapSize-agentRadius];
+% end
+% for i = (2*length(possCo)+1):min(3*length(possCo),numberOfAgents)
+%     initPositions(i,:) = [mapSize-agentRadius,-possCo(i-2*length(possCo))];
+% end
+% for i = (3*length(possCo)+1):min(4*length(possCo),numberOfAgents)
+%     initPositions(i,:) = [-possCo(i-3*length(possCo)),agentRadius-mapSize];
+% end
+goalLocations = -3.*ones(numberOfAgents,2);
 ENV.setAgentPositions(initPositions);
 ENV.setGoalPositions(goalLocations);
 ENV.setAgentVelocities(zeros(numberOfAgents,2));
@@ -35,10 +51,10 @@ ENV.setAgentVelocities(zeros(numberOfAgents,2));
 w=1;
 l=2*mapSize;
 rectangle = [-(l/2+w),0;l/2+w,0;l/2+w,-w;-(l/2+w),-w];
-% ENV.createStaticObstacle(rectangle,[0,-l/2],0,1);
-% ENV.createStaticObstacle(rectangle,[l/2,0],-pi/2,2);
-% ENV.createStaticObstacle(rectangle,[0,l/2],-pi,3);
-% ENV.createStaticObstacle(rectangle,[-l/2,0],pi/2,4);
+ENV.createStaticObstacle(rectangle,[0,-l/2],0,1);
+ENV.createStaticObstacle(rectangle,[l/2,0],-pi/2,2);
+ENV.createStaticObstacle(rectangle,[0,l/2],-pi,3);
+ENV.createStaticObstacle(rectangle,[-l/2,0],pi/2,4);
 
 
 %Optional Features
