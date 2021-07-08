230c1a9f10e56e887a23e287e0983689ba4e8094
rmb0034 <rmb0034@mix.wvu.edu>
2021-07-08 17:09:45 -0400

    agentENV Fixed Collision Detection

--- matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/debugScript.m
+++ matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/debugScript.m
@@ -2,7 +2,7 @@ clc
 clear
 close all
 %   World Building
-numberOfAgents = 10;
+numberOfAgents = 2;
 timeStep = .05;
 mapSize = 20;
 counter = 0;
@@ -10,25 +10,18 @@ f  = @testController;
 ENV = agentEnv(numberOfAgents,f,mapSize,timeStep); 
 
 %Updating agent properties
-shape = [-2,-1; -2,1; 2,1; 2,-1];%circle(.7);
+shape = circle(.7);
 for i = 1:numberOfAgents
     ENV.agents(i).setShape(shape);
     ENV.setAgentColor(i,[0 1 0]);
 end
-initPositions = zeros(numberOfAgents, 2);
-goalLocations = zeros(numberOfAgents, 2);
-for i = 1:numberOfAgents
-    theta = 2*pi/numberOfAgents * (i-1);
-    initPositions(i,:) = [cos(theta),sin(theta)]*mapSize*(1);
-    goalLocations(i,:) = [cos(theta+3.9*pi/4),sin(theta+3.9*pi/4)]*mapSize*(.9+(rand()-0.5)*.1);
-end
+initPositions = [-5,0;5,0];
+goalLocations = -initPositions;
+
 
 %Setting Initial Positions
 ENV.setAgentPositions(initPositions);
 ENV.setGoalPositions(goalLocations);
-
-%Creating Static Obstacles
-ENV.createStaticObstacle(circle(.5),[0,0],0,1);
 
 %Optional Features
 ENV.collisionsOn(true);
@@ -41,12 +34,5 @@ while(true)
     ENV.tick; 
     counter = counter + timeStep;
     fprintf("Time: %.3f \n",counter)
-    %Change goal locations
-    for i = 1:numberOfAgents
-        theta = 2*pi/numberOfAgents * (i-1) + (pi/8)*counter;
-        goalLocations(i,:) = [cos(theta+3.9*pi/4),sin(theta+3.9*pi/4)]*mapSize*(1);
-    end
     ENV.setGoalPositions(goalLocations);
 end
-
-
