932e743974bd0493372a0a777d9bfd961915a3d9
rmb0034 <rmb0034@mix.wvu.edu>
2021-07-08 10:49:20 -0400

    Update envTestDemo.m

--- matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/envTestDemo.m
+++ matlab/Agent Enviroment Sim/agentEnv 2.0/Test Scripts/envTestDemo.m
@@ -6,19 +6,15 @@ numberOfAgents = 10;
 timeStep = .05;
 mapSize = 20;
 counter = 0;
-
-f = @testController;
-
+f  = @testController;
 ENV = agentEnv(numberOfAgents,f,mapSize,timeStep); 
 
 %Updating agent properties
-shape = .4*[-2,-1;-2,1;2,1;2,-1];
+shape = [-2,-1;-2,1;2,1;2,-1];
 for i = 1:numberOfAgents
     ENV.agents(i).setShape(shape);
     ENV.setAgentColor(i,[0 1 0]);
-    ENV.updateCollisionList('A',i)
 end
-
 initPositions = zeros(numberOfAgents, 2);
 goalLocations = zeros(numberOfAgents, 2);
 for i = 1:numberOfAgents
@@ -32,22 +28,19 @@ ENV.setAgentPositions(initPositions);
 ENV.setGoalPositions(goalLocations);
 
 %Creating Static Obstacles
-shapeObs = [-1,-1;-1,1;1,1;1,-1];
 ENV.createStaticObstacle(circle(.5),[0,0],0,1);
-ENV.updateEnv; %required after each agent is finally initialized
 
 %Optional Features
 ENV.collisionsOn(true);
 ENV.pathVisibility(true);
 ENV.realTime = false;
-ENV.agentIdVisibility(false);
+ENV.agentIdVisibility(true);
 
 while(true)
     %Simulates one timestep
     ENV.tick; 
     counter = counter + timeStep;
     fprintf("Time: %.3f \n",counter)
-    
     %Change goal locations
     for i = 1:numberOfAgents
         theta = 2*pi/numberOfAgents * (i-1) + (pi/8)*counter;
