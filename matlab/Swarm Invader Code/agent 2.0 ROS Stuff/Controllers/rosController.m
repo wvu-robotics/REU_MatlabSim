218ed82fa0fb2d23646e6dd50dba155fa348582f
codinglb <85359680+codinglb@users.noreply.github.com>
2021-07-14 17:16:11 -0400

    Human Controlled Invader

--- matlab/Swarm Invader Code/agent 2.0 ROS Stuff/Controllers/rosController.m
+++ matlab/Swarm Invader Code/agent 2.0 ROS Stuff/Controllers/rosController.m
@@ -1,30 +1,33 @@
-function rosController(agent) 
+function rosController(agent)
+    agent.color = [1 0 0];
     global CURRENT_KEY_PRESSED 
     switch CURRENT_KEY_PRESSED
-        case "uparrow"
-            agent.velocityControl = 2*[0,1];
-        case "downarrow"
-            agent.velocityControl = 2*[0,-1];
-        case "rightarrow"
-            agent.velocityControl = 2*[1, 0];
-        case "leftarrow" 
-            agent.velocityControl = 2*[-1, 0];
+        case "w"
+            agent.velocityControl = 7*[0,1];
+        case "a" 
+            agent.velocityControl = 7*[-1, 0];
+        case "s"
+            agent.velocityControl = 7*[0,-1];
+        case "d"
+            agent.velocityControl = 7*[1, 0];
         case "t"
-            agent.velocityControl = 2*[-1, 1];
+            agent.velocityControl = 7*[-1, 1];
         case "y"
-            agent.velocityControl = 2*[1, 1];
+            agent.velocityControl = 7*[1, 1];
         case "g"
-            agent.velocityControl = 2*[-1, -1];
+            agent.velocityControl = 7*[-1, -1];
         case "h"
-            agent.velocityControl = 2*[1, -1];
+            agent.velocityControl = 7*[1, -1];
         case "c"
             Z = [0 0 1];
             P = [agent.pose - [0,0],0];
             T = cross(P,Z);
-            T = 0.5*(T/norm(T)); % has 3 components, only need 2 for velocity
+            T = 5*(T/norm(T)); % has 3 components, only need 2 for velocity
             agent.velocityControl = T(1:2);
         case "k"
             x = objectFlow1([1,1], agent.pose,1000000);
-            agent.velocityControl= 2*(x/norm(x));
+            agent.velocityControl= 7*(x/norm(x));
+        case "z"
+            agent.velocityControl=0;
     end
 end
