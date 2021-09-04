39c6ba41185efc42679f11031501dae56ea95b44
sej0015 <60272968+sej0015@users.noreply.github.com>
2021-09-04 19:45:48 -0400

    Added flocking and path formation

---
+++ matlab/ColorFollowers/Vector Optimization/Results Attempt/Flocking/FlockingSpawn.m
@@ -0,0 +1,24 @@
+function [initPositions, goalLocations] = FlockingSpawn(numberOfAgents, spawnType, mapSize)
+
+switch spawnType
+    case 'random'
+        initPositions = [];
+        for i = 1:numberOfAgents
+            found = false;
+            while found == false
+                initPositions(end + 1, :) = randi([-mapSize,mapSize],1,2);
+                for j = 1:size(initPositions,1) - 1
+                    found = true;
+                    if initPositions(j,:) == initPositions(end,:)
+                        found = false;
+                        initPositions(end, :) = [];
+                        break;
+                    end
+                end
+            end
+        end
+        goalLocations = zeros(numberOfAgents,2);
+end
+        
+ 
+end
