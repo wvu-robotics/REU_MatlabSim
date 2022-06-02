446902ce83efadf9049f2324186dbc46117bd8c0
pooleya19 <pooleya19@gmail.com>
2022-06-02 13:44:13 -0400

    Began implementing limited movement for boid swarm
    Rather than each boid having control of acceleration in the x and y directions, each boid has control of its forward acceleration and angular acceleration.

--- matlab/REU_2022/Topic_3_Soaring/AdamSwarmSim.m
+++ matlab/REU_2022/Topic_3_Soaring/AdamSwarmSim.m
@@ -4,17 +4,17 @@ clc
 
 %% define simulation params
 overallTime = 30; % s
-dt = .1; % s 
+dt = .05; % s 
 steps = overallTime/dt; 
 numAgents = 100;
-agentMaxVel = 1;
-neighborRadius = 5;
+agentMaxSpeed = 1;
+agentMaxOmega = pi/2;
+neighborRadius = 1.5;
 %eventually could add a dimension param(2 or more)
 
 % define agent velocity and position list
-agentPositions = zeros(steps+1,numAgents,2);
-agentVelocities = zeros(steps+1,numAgents,2);
-agentAccelerations = zeros(steps+1,numAgents,2);
+agentPositions = zeros(steps+1,numAgents,3);        %x,y,theta
+agentVelocities = zeros(steps+1,numAgents,2);       %forward,turning
 
 %% initialize to random positions
 randPosMax = 6;
@@ -22,45 +22,46 @@ for i = 1:numAgents
     %Initial positions
     agentPositions(1,i,1)= randInRange(-randPosMax,randPosMax);
     agentPositions(1,i,2)= randInRange(-randPosMax,randPosMax);
+    agentPositions(1,i,3)= randInRange(0,2*pi);
     %Initial velocities
-    agentVelocities(1,i,1)=randInRange(-agentMaxVel,agentMaxVel); 
-    agentVelocities(1,i,2)=randInRange(-agentMaxVel,agentMaxVel);
-    %Initial accelerations = 0
+    agentVelocities(1,i,1)=randInRange(0,agentMaxSpeed); 
+    agentVelocities(1,i,2)=randInRange(-agentMaxOmega,agentMaxOmega);
 end
 
 %% setup video and figure
-video = VideoWriter('temp2.avi');
+video = VideoWriter('temp4.avi');
 video.FrameRate = 1/dt;
 open(video);
 
-fig = figure('Visible','on');
+fig = figure('Visible','off');
 
 %% run simulation
 for step = 1:steps
     fprintf("Frame %g/%g\n",step,steps);
     clf
-    currentPositions = agentPositions(step,:,:);
     
     % plot current positions
-    xdata = currentPositions(:,:,1);
-    ydata = currentPositions(:,:,2);
+    xdata = agentPositions(step,:,1);
+    ydata = agentPositions(step,:,2);
     scatter(xdata,ydata,50,'filled','black');
     hold on
     
-    graphScale = 1.25;
-    xlim([-graphScale*randPosMax graphScale*randPosMax])
-    ylim([-graphScale*randPosMax graphScale*randPosMax])
+    graphScale = 1.75;
+    wallMag = graphScale*randPosMax;
+    xlim([-wallMag wallMag])
+    ylim([-wallMag wallMag])
     daspect([1 1 1])
     
     %Simulate each agent
     for agent=1:numAgents
         %Step through simulation
-        [newAccel, newVel, newPos] = stepSim(agentPositions, agentVelocities, agentAccelerations, step, agent, dt, numAgents, neighborRadius);
-        %Update next positions, velocities, and accelerations
-        for i=1:2
+        [newAccel, newVel, newPos] = stepSim(agentPositions, agentVelocities, step, agent, dt, numAgents, neighborRadius, wallMag);
+        %Update next positions, velocities
+        for i=1:3
            agentPositions(step+1,agent,i) = newPos(i);
-           agentVelocities(step+1,agent,i) = newVel(i);
-           agentAccelerations(step+1,agent,i) = newAccel(i);
+        end
+        for i=1:2
+            agentVelocities(step+1,agent,i) = newVel(i);
         end
     end
 
@@ -77,24 +78,33 @@ close(video);
 
 
 %% define simulation step function
-function [newAccel, newVel, newPos] = stepSim(positions, velocities, accelerations, step, agent, dt, numAgents, neighborRadius)
+function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agent, dt, numAgents, neighborRadius, wallMag)
     %This squeeze method returns 2x1 matrices
-    agentPos = squeeze(positions(step,agent,:));
+    agentPos = squeeze(positions(step,agent,1:2));
+    agentTheta = positions(step,agent,3);
     agentVel = squeeze(velocities(step,agent,:));
-    agentAccel = squeeze(accelerations(step,agent,:));
     
-    newAccel = [0;0];
+    tempAccel = [0;0];
     
-    maxAccel = 2;
-    maxVel = 1;
-
-    %Iterate through all agents
+    maxForwardAccel = 20;
+    maxAlpha = pi;
+    maxForwardVel = 1.5;
+    maxOmega = pi;
+    
+    separation = 5;
+    cohesion = 1.5;
+    alignment = 3;
+    separationWall = 300;
+    
+    %Iterate through all other agents
     for other = 1:numAgents
         if (other == agent)
             continue;
         end
         
-        otherPos = squeeze(positions(step,other,:));
+        otherPos = squeeze(positions(step,other,1:2));
+        otherTheta = positions(step,other,3);
+        otherVelUnit = [cos(otherTheta);sin(otherTheta)];
         
         diffPos = otherPos - agentPos;
         distToOther = norm(diffPos);
@@ -104,30 +114,58 @@ function [newAccel, newVel, newPos] = stepSim(positions, velocities, acceleratio
             continue;
         end
         
-        separation = 1;
-        cohesion = 0.8;
+        accelMag_separation = separation * -1/distToOther^2; %Negative, to go AWAY from the other
+        accelMag_cohesion = cohesion * distToOther^2; %Positive, to go TOWARDS the other
+        accelMag_alignment = alignment * 1/distToOther^2;
         
-        accelMag_separation = -1/distToOther^2; %Negative, to go AWAY from the other
-        accelMag_cohesion = 1/(distToOther-neighborRadius)^2; %Positive, to go TOWARDS the other
-        accelMag = separation*accelMag_separation + cohesion*accelMag_cohesion;
-        
-        accel = accelMag * diffUnit;
-        newAccel = newAccel + accel;
+        accel = accelMag_separation*diffUnit + accelMag_cohesion*diffUnit + accelMag_alignment*otherVelUnit;
+        tempAccel = tempAccel + accel;
     end
+    
+    %Enforce wall separation
+    wallPoints = [[wallMag;agentPos(2)],[agentPos(1);wallMag],[-wallMag;agentPos(2)],[agentPos(1);-wallMag]];
+    separationWallAccel = [0;0];
+    for i=1:4
+       wallPoint = wallPoints(:,i);
+       diffPos = wallPoint - agentPos;
+       distToWall= norm(diffPos);
+       if(distToWall > neighborRadius)
+           continue;
+       end
+       diffUnit = diffPos / distToWall;
+       accelMag_separationWall = separationWall * -1/distToWall^4;
+       if(accelMag_separationWall > norm(separationWallAccel))
+           separationWallAccel = accelMag_separationWall * diffUnit;
+       end
+    end
+    tempAccel = tempAccel + separationWallAccel;
     
     %Enforce boundary conditions
-    newAccelMag = norm(newAccel);
-    if(newAccelMag > maxAccel)
-        newAccel = newAccel / newAccelMag * maxAccel;
+    forwardUnit = [cos(agentTheta);sin(agentTheta)];
+    newAccel_forward = dot(tempAccel,forwardUnit);
+    newAccel_alpha = 1*sqrt((norm(tempAccel))^2-newAccel_forward^2);
+    newAccel = [newAccel_forward; newAccel_alpha];
+    
+    if(norm(newAccel(1)) > maxForwardAccel)
+       newAccel(1) = sign(newAccel(1)) * maxForwardAccel; 
     end
     
-    newVel = agentVel + newAccel*dt;
-    newVelMag = norm(newVel);
-    if(newVelMag > maxVel)
-        newVel = newVel / newVelMag * maxVel;
+    if(norm(newAccel(2)) > maxAlpha)
+        newAccel(2) = sign(newAccel(2)) * maxAlpha;
     end
     
-    newPos = agentPos + newVel*dt;
+    newVel = agentVel + tempAccel*dt;
+    if(norm(newVel(1)) > maxForwardVel)
+        newVel(1) = sign(newVel(1)) * maxForwardVel;
+    end
+    
+    if(norm(newVel(2)) > maxOmega)
+        newVel(2) = sign(newVel(2)) * maxOmega;
+    end
+    
+    newPos = agentPos + newVel(1)*forwardUnit*dt;
+    newTheta = agentTheta + newVel(2)*dt;
+    newPos(3) = newTheta;
 end
 
 %% define velocity function
