c89e04278b889d086df4211166286ae2b0390941
MaxlGao <100160681+MaxlGao@users.noreply.github.com>
2022-06-03 13:38:19 -0400

    Added basic thermal effects

--- matlab/REU_2022/Topic_3_Soaring/MaxSwarmSim.m
+++ matlab/REU_2022/Topic_3_Soaring/MaxSwarmSim.m
@@ -3,37 +3,40 @@ clear
 clc
 
 %% define simulation params
-% Units mi/ft lb s + kt
-% 1 kt = 1.687 ft/s = 1.151 mph
+% Units mi/ft lb min + kt
+% 1 kt = 1.151 mph
 % 1 mi = 5280 ft
 % xy in mi, alti in ft
-overallTime = 60; % s
-dt = .05; % s 
+% Simulation runs at exactly 60x real time.
+overallTime = 5; % min
+dt = .05; % min
 steps = overallTime/dt; 
-numAgents = 100;
+numAgents = 20;
 
 maxForwardAccel = 20;
 maxAlpha = 2*pi;
-maxForwardVel = 1.5;
-minForwardVel = 1;
-maxOmega = pi;
-sinkRate = 120;
-
+maxForwardVel = 1.6;
+minForwardVel = 1.1;
+maxOmega = 2*pi/3;
 separation = 5;
 cohesion = 1.5;
 alignment = 3;
 separationWall = 10;
-
 neighborRadius = 1.5;
+sinkRate = 500;
+
 agentScale = 2;
 
-thermalWidth = 0.2;
+% X position, Y position, Width, Rise Rate, Ceiling
+thermal = [0,0,8,600,8000];
 
-simParams = [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, separation, cohesion, alignment, separationWall, neighborRadius];
+simParams = [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, ...
+             separation, cohesion, alignment, separationWall, neighborRadius, sinkRate];
 
 % define agent velocity and position list
 agentPositions = zeros(steps+1,numAgents,4);        %x,y,theta, altitude
 agentVelocities = zeros(steps+1,numAgents,2);       %forward,turning
+%agentTelemetry = [agentPositions;agentVelocities];
 
 %% initialize to random positions
 randPosMax = 6;
@@ -68,7 +71,7 @@ for step = 1:steps
     %ydata = agentPositions(step,:,2);
     %scatter(xdata,ydata,50,'filled','black');
     currentPositions = (squeeze(agentPositions(step,:,:)))';
-    renderBoids(currentPositions,numAgents,Ceiling,agentScale);
+    renderBoids(currentPositions,numAgents,Ceiling,agentScale,thermal);
     hold on
     
     graphScale = 1.75;
@@ -81,7 +84,7 @@ for step = 1:steps
     for agent=1:numAgents
         if agentPositions(step,agent,4) > 0 % agent must not be dead
             %Step through simulation
-            [newAccel, newVel, newPos] = stepSim(agentPositions, agentVelocities, step, agent, dt, numAgents, wallMag, simParams, sinkRate);
+            [newAccel, newVel, newPos] = stepSim(agentPositions, agentVelocities, step, agent, dt, numAgents, wallMag, simParams,thermal);
             %Update next positions, velocities
             for i=1:4
                 agentPositions(step+1,agent,i) = newPos(i);
@@ -96,7 +99,6 @@ for step = 1:steps
     hold off
     currFrame = getframe(fig);
     writeVideo(video,currFrame);
-
     %convert this to realtime
     pause(0.0001);
 end
@@ -104,7 +106,7 @@ end
 close(video);
 toc
 %% define simulation step function
-function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agent, dt, numAgents, wallMag, simParams, sinkRate)
+function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agent, dt, numAgents, wallMag, simParams,thermal)
     %This squeeze method returns 2x1 matrices
     agentPos = squeeze(positions(step,agent,1:2));
     agentTheta = positions(step,agent,3);
@@ -113,7 +115,7 @@ function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agen
     
     tempAccel = [0;0];
     
-    [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, separation, cohesion, alignment, separationWall, neighborRadius] = unpack(simParams);
+    [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, separation, cohesion, alignment, separationWall, neighborRadius, sinkRate] = unpack(simParams);
     
     %Iterate through all other agents
     for other = 1:numAgents
@@ -140,7 +142,8 @@ function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agen
         accel = accelMag_separation*diffUnit + accelMag_cohesion*diffUnit + accelMag_alignment*otherVelUnit;
         tempAccel = tempAccel + accel;
     end
-    
+    % End interaction
+
     %Enforce wall separation
     wallPoints = [[wallMag;agentPos(2)],[agentPos(1);wallMag],[-wallMag;agentPos(2)],[agentPos(1);-wallMag]];
     wallAccelUnits = [[-1;0],[0;-1],[1;1],[0;1]];
@@ -156,6 +159,7 @@ function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agen
        tempAccel = tempAccel + accelMag_separationWall * wallAccelUnits(:,i);
        wallAccel = wallAccel + accelMag_separationWall * wallAccelUnits(:,i);
     end
+    % End wall
     
     %Enforce boundary conditions
     forwardUnit = [cos(agentTheta);sin(agentTheta)];
@@ -195,6 +199,11 @@ function [tempAccel, newVel, newPos] = stepSim(positions, velocities, step, agen
     newTheta = agentTheta + newVel(2)*dt;
     newPos(3) = newTheta;
     newPos(4) = agentAlti - sinkRate*dt;
+
+    % giga jank
+    if norm(newPos(1),newPos(2)) < thermal(3)/2 && newPos(4) < thermal(5)
+        newPos(4) = newPos(4) + thermal(4)*dt;
+    end
     %fprintf("Agent %g: Pos(%g,%g,%g) Vel(%g,%g) Accel(%g,%g) WallAccelGlobal(%g,%g)\n",agent,newPos(1),newPos(2),newPos(3),newVel(1),newVel(2),newAccel(1),newAccel(2),wallAccel(1),wallAccel(2));
 end
 %% Utility Functions
@@ -202,7 +211,7 @@ function num = randInRange(a,b)
     num = rand(1)*(b-a) + a;
 end
 
-function [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, separation, cohesion, alignment, separationWall, neighborRadius] = unpack(simParams)
+function [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, separation, cohesion, alignment, separationWall, neighborRadius, sinkRate] = unpack(simParams)
     maxForwardAccel = simParams(1);
     maxAlpha = simParams(2);
     maxForwardVel = simParams(3);
@@ -213,11 +222,12 @@ function [maxForwardAccel, maxAlpha, maxForwardVel, minForwardVel, maxOmega, sep
     alignment = simParams(8);
     separationWall = simParams(9);
     neighborRadius = simParams(10);
+    sinkRate = simParams(11);
 end
 
 %% Render Boids
 %Assume positions = 3 x numAgents matrix
-function renderBoids(positions,numAgents,Ceiling,agentScale)
+function renderBoids(positions,numAgents,Ceiling,agentScale,thermal)
     %Define relative boid shape = x-values...; y-values...
     boidShape = agentScale.*[-.5, .5, -.5;
                  -.5, 0, .5];
@@ -225,6 +235,7 @@ function renderBoids(positions,numAgents,Ceiling,agentScale)
     boidShape(2,:) = boidShape(2,:) * 0.3;
     normAltitude = abs(positions(4,:)./Ceiling.*0.6);
     boidColor = hsv2rgb([normAltitude',ones(1,numAgents)',ones(1,numAgents)']);
+    rectangle('Position',[-thermal(3)/2,-thermal(3)/2,thermal(3),thermal(3)],'Curvature',1,'FaceColor',[1,.5,.5]);
     for agent=1:numAgents
         if positions(4,agent) > 0
             position = positions(:,agent);
@@ -237,6 +248,7 @@ function renderBoids(positions,numAgents,Ceiling,agentScale)
 
             %Render polygon
             patch(globalBoidShape(1,:),globalBoidShape(2,:),boidColor(agent,:));
+            
         end
                
     end
