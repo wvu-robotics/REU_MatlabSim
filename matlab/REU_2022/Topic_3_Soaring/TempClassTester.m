c5c55f63ea3b1f33140b2bd93b95e15c0e996bdf
pooleya19 <pooleya19@gmail.com>
2022-06-09 11:40:55 -0400

    Updated test classes using handles

--- matlab/REU_2022/Topic_3_Soaring/TempClassTester.m
+++ matlab/REU_2022/Topic_3_Soaring/TempClassTester.m
@@ -5,6 +5,16 @@ clc
 separation = 1.0;
 cohesion = 2.0;
 alignment = 3.0;
+listClasses(1,10) = TempClass(); %Fill matrix with default values
+for i = 1:10
+    listClasses(1,i).packData(i,i^2,i^3);
+end
+
+for i = 1:10
+    listClasses(1,i).print();
+end
+
+%{
 temp = TempClass(separation,cohesion,alignment);
 printInfo(temp);
 
@@ -12,3 +22,4 @@ function printInfo(tempClass)
     [separation, cohesion, alignment] = tempClass.unpackData();
     fprintf("Separation: %g\nCohesion: %g\nAlignment: %g\n",separation, cohesion, alignment);
 end
+%}
