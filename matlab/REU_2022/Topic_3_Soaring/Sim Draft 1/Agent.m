c30431fbde62fee8eebad9bfa17acbc2c08aceca
pooleya19 <pooleya19@gmail.com>
2022-06-09 14:49:35 -0400

    Updated SimLaw and Agent
    Initially added list of variables to SimLaw. Added variables and function header to Agent.

--- matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Agent.m
+++ matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Agent.m
@@ -1,1 +1,16 @@
 % Agent class
+classdef Agent < handle
+    properties
+        position = [0.0, 0.0, 0.0]      %m, [x,y,z]
+        heading = 0.0                   %rad
+        bankAngle = 0.0                 %rad
+        velocity = [0.0, 0.0]           %m/s, rad/s, [forward,omega]
+        fov = 2*pi                      %rad
+    end
+    
+    methods
+        function obj = update(obj,localAgents,thermalStrength)
+            
+        end
+    end
+end
--- matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/SimLaw.m
+++ matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/SimLaw.m
@@ -1,1 +1,34 @@
 % Simulation parameter class
+classdef SimLaw
+    properties
+        % Simulation constraints
+        dt = 0.1        %s
+        totalTime = 60  %s
+        mapSize = 200   %m, side of square map
+        numAgents = 50  %agents
+        numThermals = 2 %thermals
+        
+        % Initial conditions
+        agentSpawnPosRange = [-70,-70; 70,70];     %m, [xMin,yMin;xMax,yMax]
+        agentSpawnVelRange = [8,0;13,0];           %m/s,rad/s [forwardMin,omegaMin;forwardMax,omegaMax];
+        
+        % Rule constraints
+        separation = 1.0
+        cohesion = 1.0
+        alignment = 1.0
+        migration = 1.0
+        
+        % Agent constraints
+        neighborRadius = 20     %m
+        forwardSpeedMin = 5     %m/s
+        forwardSpeedMax = 18    %m/s
+        forwardInertia = 10
+        omegaMin = -pi          %rad/s
+        omegaMax = pi           %rad/s
+        omegaInertia = 1
+        
+        % Thermal constraints
+        thermalSpeedMin = 0.5   %m/s
+        thermalSpeedMax = 2.0   %m/s
+    end
+end
