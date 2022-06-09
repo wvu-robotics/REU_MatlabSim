a3c05c63f8897841057318501463e74ff4125d4b
Malegoca <mag0053@mix.wvu.edu>
2022-06-09 14:47:00 -0400

    refined controllers and add periodic boundaries

--- matlab/REU_2022/Topic_1_ Imitating_Swarms/MSIM/create_swarm.m
+++ matlab/REU_2022/Topic_1_ Imitating_Swarms/MSIM/create_swarm.m
@@ -1,14 +1,15 @@
-function AGENTS = create_swarm(Nagents,map_length,range)
+function AGENTS = create_swarm(Nagents,mapsize,range)
 
+mapsize=mapsize-5;
 AGENTS = agent.empty();
-gains = [10,5,10];
+gains = [3,3,0.5];
 Ka = gains(1); % Alingment gain
 Ks = gains(2); % Separation gain
 Kc = gains(3); % Cohesion gain
 
 for i=1:Nagents
-   x = 2*rand*map_length - map_length;
-   y = 2*rand*map_length - map_length;
+   x = randi([-mapsize mapsize]);
+   y = randi([-mapsize mapsize]);
    AGENTS(i)=agent(x,y,Ks,Ka,Kc,i);
    AGENTS(i).range = range;
    AGENTS(i).ID = i;
