2160951034cf546aa63c19e01cde9f668fd60ec8
MaxlGao <100160681+MaxlGao@users.noreply.github.com>
2022-06-10 09:01:12 -0400

    Update Utility.m

--- matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Utility.m
+++ matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Utility.m
@@ -5,5 +5,20 @@ classdef Utility
         function value = randIR(low,high) 
             value = rand() * (high-low) + low;
         end
+        
+        % Calculates distance if within threshold.
+        function [Verdict] = isNear(A1, A2, Threshold)
+            Verdict = NaN;
+            if abs(A1.pos(1) - A2.pos(1)) > Threshold 
+                return
+            elseif abs(A1.pos(2) - A2.pos(2)) > Threshold
+                return
+            end
+            
+            dist = norm([(A1.pos(1)-A2.pos(1)), (A1.pos(2)-A2.pos(2))]);
+            if dist < Threshold
+                Verdict = dist;
+            end
+        end
     end
 end
