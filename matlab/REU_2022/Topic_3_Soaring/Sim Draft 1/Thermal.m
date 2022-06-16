9c8610684fdd40da84989df2c94a15be120e5daa
Sachi <sobarnaby24@gmail.com>
2022-06-16 15:33:45 -0400

    Added static thermal sim and improved ThermalTest

--- matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Thermal.m
+++ matlab/REU_2022/Topic_3_Soaring/Sim Draft 1/Thermal.m
@@ -7,30 +7,41 @@ classdef Thermal < handle
         maxStrength = 0;
         curStrength = 0;
         stepCount = 0;
-        strengthDirection = 1;
+        strengthDirection = -1;
         bounds = [0 0];
     end
     methods
-        % Default constructor for Thermal
-        function thermal = Thermal(simLaw)
+        % Different constructor for Thermal
+        function thermal = Thermal(simLaw, velocity)
             thermalMap.simLaw = simLaw;
             SL = thermalMap.simLaw;
-
-            % Randomize the thermal's properties
-            thermal.position(1) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
-            thermal.position(2) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
-                    
-            % Randomly decide if the velocity is negative or positive
-            randFactor = randi([0 1],1,2);
-            randFactor(randFactor == 0) = -1;
-            thermal.velocity(1) = Utility.randIR(SL.thermalSpeedMin,SL.thermalSpeedMax)*randFactor(1);
-            thermal.velocity(2) = Utility.randIR(SL.thermalSpeedMin,SL.thermalSpeedMax)*randFactor(2);
-                    
-            thermal.radius = round(Utility.randIR(SL.thermalRadiusMin,SL.thermalRadiusMax));
-            thermal.maxStrength = round(Utility.randIR(SL.thermalStrengthMin + 1,SL.thermalStrengthMax));
-            thermal.curStrength = round(Utility.randIR(1, thermal.maxStrength));
-        
-            thermal.bounds = [SL.mapSize(1)+thermal.radius+1 SL.mapSize(2)-thermal.radius-1];
+            if nargin == 1 % Default constructor 
+                % Randomize the thermal's properties
+                thermal.position(1) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
+                thermal.position(2) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
+                        
+                % Randomly decide if the velocity is negative or positive
+                randFactor = randi([0 1],1,2);
+                randFactor(randFactor == 0) = -1;
+                thermal.velocity(1) = Utility.randIR(SL.thermalSpeedMin,SL.thermalSpeedMax)*randFactor(1);
+                thermal.velocity(2) = Utility.randIR(SL.thermalSpeedMin,SL.thermalSpeedMax)*randFactor(2);
+                        
+                thermal.radius = round(Utility.randIR(SL.thermalRadiusMin,SL.thermalRadiusMax));
+                thermal.maxStrength = round(Utility.randIR(SL.thermalStrengthMin + 1,SL.thermalStrengthMax));
+                thermal.curStrength = round(Utility.randIR(1, thermal.maxStrength));
+            
+                thermal.bounds = [SL.mapSize(1)+thermal.radius+1 SL.mapSize(2)-thermal.radius-1];
+            elseif nargin == 2 % When velocity is specified
+                % Randomize the thermal's properties
+                thermal.position(1) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
+                thermal.position(2) = round(Utility.randIR(SL.mapSize(1) + SL.thermalRadiusMax,SL.mapSize(2) - SL.thermalRadiusMax));
+                     
+                thermal.radius = round(Utility.randIR(SL.thermalRadiusMin,SL.thermalRadiusMax));
+                thermal.maxStrength = round(Utility.randIR(SL.thermalStrengthMin + 1,SL.thermalStrengthMax));
+                thermal.curStrength = round(Utility.randIR(1, thermal.maxStrength-1));
+            
+                thermal.bounds = [SL.mapSize(1)+thermal.radius+1 SL.mapSize(2)-thermal.radius-1];
+            end
         end
     end
 end
